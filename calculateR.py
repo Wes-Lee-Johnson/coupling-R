6b7e3938c7697e5623bf86c8838b9d0c41bad37d
Wes-Lee-Johnson <quantumcat.wj@gmail.com>
2023-03-23 15:30:54 -0600

    removed functions from file

--- calculateR.py
+++ calculateR.py
@@ -1,21 +1,16 @@
 """
 Author: Wes Johnson
-Date:   03112
+Date:   032323
 Purpose:
-        This Script analyzes the simulation by loading the pickled 
-        mode analyist code and saved '.npy' files containing the 
-        trajectories of the ions in 6D phase space. 
-        Contains functions to calculate the energy in rotating frame 
-        indepedent of mode analysis.
-
-        Analyzes data for 4ion parameter scan over offset and width 
-        of perp. laser.
+        This script compares the trapping frequencies need to achieve 
+        particular values of beta and R
 """
 #cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
 # imports 
 import numpy as np
 import matplotlib.pyplot as plt 
 from scipy import constants 
+import R_mod 
 
 #cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
 
@@ -26,75 +21,32 @@ q   = constants.e
 wz  = 2*np.pi * 116e3
 wc  = q/m*B
 wr  = 2*np.pi * 23.5e3 
-#cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
-
-# functions 
-#def calc_wc(q,m,B): 
-#    """unmodified cyclotron frequency
-#        q   :: charge 
-#        m   :: mass 
-#        B   :: magnetic feild
-#    """
-#    return q/m*B 
-
-def calc_wz_b(wr,wc=q/m*B,b=0.0348): 
-    return np.sqrt( (wr*wc - wr**2)/(b + 1/2) )
-
-def calc_wz_R(wr,wc=q/m*B,R=5): 
-    a   = (wc - 2*wr)*(R + 1)/(R - 1)
-    return np.sqrt( (1/2)*(wc**2 - a**2) )
-
-
-def findMinDiff(x,y,z):
-    diff    = np.abs(x-y)
-    notnan  = np.logical_not((np.isnan(diff)))
-    mindex  = diff==np.min(diff[notnan])
-    return  1/2*(x[mindex]+y[mindex]),z[mindex]
-
-def calc_wr(wc,R,b): 
-    RpRm= ((R + 1)/(R - 1))**2  
-    C   = 1/2*wc**2*(1 - RpRm)
-    B   = wc*(1/(b + 1/2) - 2*RpRm)
-    A   = B/wc 
-    return (B + np.sqrt(B**2 - 4*A*C))/(2*A)
-
-def calc_wpm(wz,wc):
-    dw  = np.sqrt(wc**2 - 2*wz**2)
-    wp  = 1/2*(wc + dw)
-    wm  = 1/2*(wc - dw)
-    return wp,wm 
-
-def calc_R(wz,wc,wr): 
-    wp,wm   = calc_wpm(wz,wc) 
-    return abs((wp - wr)/(wm - wr))
-
-def calc_b(wz,wc,wr): 
-    beta = (wr*wc - wr**2)/(wz**2) - 1/2
-    return beta 
 
 #cc05ccc10ccc15ccc20ccc25ccco30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
-b   = calc_b(wz,wc,wr)
-print('R=',calc_R(wz,wc,wr))
+#parameters for 40Ca+
+b   = R_mod.calc_b(wz,wc,wr)
+print('R=',R_mod.calc_R(wz,wc,wr))
 print('b=',b)
 print('\n')
-wr  = calc_wr(wc,5,b)
+wr  = R_mod.calc_wr(wc,5,b)
 print('fr=',wr/2/np.pi/1e3)
-print('fz=',calc_wz_b(wr,wc,b)/2/np.pi/1e3)
+print('fz=',R_mod.calc_wz_b(wr,wc,b)/2/np.pi/1e3)
 print('\n')
 #cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
 
 
 
 #cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
+#parameters for 9Be+
 wz  = 2*np.pi * 1.58e6 
 m   = 9.012*constants.u 
 B   = 4.4588
 wc  = q/m*B 
 wr  = 2*np.pi * 180e3
 
-print('R=',calc_R(wz,wc,wr))
-print('b=',calc_b(wz,wc,wr))
-print('fr=',calc_wr(wc,5,calc_b(wz,wc,wr))/2/np.pi/1e3)
+print('R=',R_mod.calc_R(wz,wc,wr))
+print('b=',R_mod.calc_b(wz,wc,wr))
+print('fr=',R_mod.calc_wr(wc,5,R_mod.calc_b(wz,wc,wr))/2/np.pi/1e3)
 exit()
 #cc05ccc10ccc15ccc20ccc25ccc30ccc35ccc40ccc45ccc50ccc55ccc60ccc65ccc70cc
 
